bc74a8f0c10c5a58ae1019ed73fbcf7fa109fa7a
Aries <me.mory@qq.com>
2017-12-24 16:39:08 +0800

    Initial commit

---
+++ app/src/main/java/com/aries/opengl/MainActivity.java
@@ -0,0 +1,52 @@
+package com.aries.opengl;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.aries.opengl.helloworld.OpenGLRenderer;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE); // (NEW)
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); // (NEW)
+
+        GLSurfaceView mGLSurfaceView = new GLSurfaceView(this);
+        if ( detectOpenGLES30() )
+        {
+            // Tell the surface view we want to create an OpenGL ES 3.0-compatible
+            // context, and set an OpenGL ES 3.0-compatible renderer.
+            mGLSurfaceView.setEGLContextClientVersion ( 3 );
+            // Set the renderer to our demo renderer, defined below.
+            mGLSurfaceView.setRenderer(new OpenGLRenderer());
+        }
+        else
+        {
+            // This is where you could create an OpenGL ES 2.0 and/or 1.x compatible
+            // renderer if you wanted to support both ES 1 and ES 2.
+            Log.e("HelloTriangle", "OpenGL ES 3.0 not supported on device. Exiting...");
+            finish();
+        }
+        setContentView(mGLSurfaceView);
+    }
+
+    private boolean detectOpenGLES30()
+    {
+        ActivityManager am =
+                ( ActivityManager ) getSystemService ( Context.ACTIVITY_SERVICE );
+        ConfigurationInfo info = am.getDeviceConfigurationInfo();
+        return ( info.reqGlEsVersion >= 0x30000 );
+    }
+}
