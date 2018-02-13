4df60c17e1ba92e36688f1d5797d58c5c3520c36
jiangyidong <me.mory@qq.com>
2018-02-13 17:59:03 +0800

    [add]添加OpenGL2教程--有问题

--- app/src/main/java/com/aries/opengl/MainActivity.java
+++ app/src/main/java/com/aries/opengl/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.ActivityManager;
 import android.content.Context;
 import android.content.pm.ConfigurationInfo;
 import android.opengl.GLSurfaceView;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,8 +12,11 @@ import android.view.Window;
 import android.view.WindowManager;
 
 import com.aries.opengl.helloworld.OpenGLRenderer;
+import com.aries.opengl.helloworld.TriangleRenderer;
 
 public class MainActivity extends AppCompatActivity {
+
+    GLSurfaceView mGLSurfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +28,18 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN); // (NEW)
 
         GLSurfaceView mGLSurfaceView = new GLSurfaceView(this);
-        if ( detectOpenGLES30() )
-        {
+//        if (checkSupportedOpenGLES20()) {
+//            mGLSurfaceView.setEGLContextClientVersion ( 2 );
+//            mGLSurfaceView.setRenderer(new TriangleRenderer());
+//        } else
+        if (detectOpenGLES30()) {
             // Tell the surface view we want to create an OpenGL ES 3.0-compatible
             // context, and set an OpenGL ES 3.0-compatible renderer.
             mGLSurfaceView.setEGLContextClientVersion ( 3 );
+//            mGLSurfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
             // Set the renderer to our demo renderer, defined below.
             mGLSurfaceView.setRenderer(new OpenGLRenderer());
-        }
-        else
-        {
+        } else {
             // This is where you could create an OpenGL ES 2.0 and/or 1.x compatible
             // renderer if you wanted to support both ES 1 and ES 2.
             Log.e("HelloTriangle", "OpenGL ES 3.0 not supported on device. Exiting...");
@@ -52,5 +58,37 @@ public class MainActivity extends AppCompatActivity {
                 ( ActivityManager ) getSystemService ( Context.ACTIVITY_SERVICE );
         ConfigurationInfo info = am.getDeviceConfigurationInfo();
         return ( info.reqGlEsVersion >= 0x30000 );
+    }
+
+    private boolean checkSupportedOpenGLES20() {
+        ActivityManager activityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x2000;
+
+        boolean isEmulator = Build.VERSION.SDK_INT > Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1
+                && (Build.FINGERPRINT.startsWith("generic")
+                || Build.FINGERPRINT.startsWith("unknown")
+                || Build.MODEL.contains("google_sdk")
+                || Build.MODEL.contains("Emulator")
+                || Build.MODEL.contains("Android SDK built for x86"));
+
+        supportsEs2 = supportsEs2 || isEmulator;
+        return supportsEs2;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mGLSurfaceView != null) {
+            mGLSurfaceView.onPause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mGLSurfaceView != null) {
+            mGLSurfaceView.onResume();
+        }
     }
 }
